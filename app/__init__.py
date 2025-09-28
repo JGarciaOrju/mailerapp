63e74e87c58e92418caf14c9f94528c73cbb98cb
JGarciaOrju <julius1196@hotmail.com>
2025-09-28 12:59:20 -0500

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_mapping(
+        FROM_EMAIL=os.environ.get("FROM_EMAIL"),
+        SENDGRID_KEY=os.environ.get("SENDGRID_API_KEY", ""),
+        SECRET_KEY=os.environ.get("SECRET_KEY", "dev"),
+        DATABASE_HOST=os.environ.get("FLASK_DATABASE_HOST", "localhost"),
+        DATABASE_USER=os.environ.get("FLASK_DATABASE_USER", "root"),
+        DATABASE_PASSWORD=os.environ.get("FLASK_DATABASE_PASSWORD", ""),
+        DATABASE=os.environ.get("FLASK_DATABASE", "mailerapp"),
+    )
+
+    from . import db
+
+    db.init_app(app)
+
+    from . import mail
+
+    app.register_blueprint(mail.bp)
+    return app
